e61b7a64db992cee146aad2b1b74c585c8d16f86
Kasim-Dawood <kasimdahoud@gmail.com>
2021-01-19 20:32:55 +0100

    all done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.destroy_all
+# Cocktail.destroy_all
+# Dose.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Moji")
+Cocktail.create(name: "Bloody Mar")
+Cocktail.create(name: "The Beach")
+
+Dose.create(cocktail_id: 9, ingredient_id: 16, description: "Mojito")
+Dose.create(cocktail_id: 10, ingredient_id: 17, description: "Bloody Mary")
+Dose.create(cocktail_id: 11, ingredient_id: 18, description: "Sex on the Beach")
+
+# Ingredient.all
+# Ingredient.count
